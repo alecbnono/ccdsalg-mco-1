1217556aa234d8e394e7be819a651a9741e112e5
Alec Nono <alecbnono@gmail.com>
2025-05-28 18:55:48 +0800

    Add stack.h with lines

---
+++ stack.h
@@ -0,0 +1,21 @@
+#define MAX_ELEMENTS 32768
+
+typedef struct 
+{
+    int x;
+    int y;
+} Point;
+
+typedef struct
+{
+    Point data[MAX_ELEMENTS];
+    int top;
+} Stack;
+
+void stackCreate(Stack *S);
+void stackPush(Stack *S, Point x);
+void stackPop(Stack *S);
+Point stackTop(Stack *S);
+int stackIsFull(Stack *S);
+int stackIsEmpty(Stack *S);
+Point stackNextToTop(Stack *S);
