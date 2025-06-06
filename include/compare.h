602ec0b964c068c6710e59820ad0c8a4d01083fc
awsuj <68768061+awsuj@users.noreply.github.com>
2025-06-06 11:58:59 +0800

    Update compare.h

--- include/compare.h
+++ include/compare.h
@@ -1,3 +1,10 @@
 #include "stack.h"
 
-int compare(Point a, Point b);
+int compare(Point a, Point b) {
+    if (a.x < b.x) return -1;
+    if (a.x > b.x) return 1;
+    if (a.y < b.y) return -1;
+    if (a.y > b.y) return 1;
+    return 0;
+}
+
