4bd8b1e9d120c4b53aefceaa9d8b06ec66d879bc
awsuj <68768061+awsuj@users.noreply.github.com>
2025-06-06 11:49:26 +0800

    Update qsort.c

--- src/qsort.c
+++ src/qsort.c
@@ -1,4 +1,37 @@
 #include "qsort.h"
 
+void swap(Point *a, Point * b) 
+{
+    Point temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
+int partition(Point arr[], int low, int high) 
+{
+    Point pivot = arr[high];
+    int i = low - 1;
 
+    for (int j = low; j < high; j++) {
+        if (compare_points(arr[j], pivot) < 0) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quickSort(Point arr[], int low, int high) 
+{   
+    int pi;
+
+    if (low < high) 
+    {
+     pi = partition(arr, low, high);   
+    }
+
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+}
