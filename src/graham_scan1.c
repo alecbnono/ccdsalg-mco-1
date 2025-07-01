9afa56ee71caee65aa5f546c3772e2a901c56ead
awsuj <68768061+awsuj@users.noreply.github.com>
2025-07-01 14:42:56 +0800

    Update graham_scan1.c

---
+++ src/graham_scan1.c
@@ -0,0 +1,25 @@
+#include "utils.h"
+
+void slowGrahamScan(Stack *input, Stack *result)
+{
+    int P0 = getMinYIndex(input);
+
+    selectionSort(input, input->top, P0);
+
+    // pushing initial points into result stack
+    result->top = -1;
+    result->data[++result->top] = input->data[0];
+    result->data[++result->top] = input->data[1];
+    result->data[++result->top] = input->data[2];
+
+    // comparison of remaining points
+    for (int i = 3; i <= input->top; i++) {
+        while (result->top >= 1 &&
+               isCounterClockwise(result->data[result->top - 1],
+                                  result->data[result->top],
+                                  input->data[i]) <= 0) {
+            result->top--;
+        }
+        result->data[++result->top] = input->data[i];
+    }
+}
