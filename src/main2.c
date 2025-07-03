22f9d2758a0466ba455e734c4d49f09ce6322877
Alec Nono <alecbnono@gmail.com>
2025-07-03 20:38:48 +0800

    Add relevant files

--- src/main2.c
+++ src/main2.c
@@ -1,3 +1,20 @@
 #include "../include/stack.h"
 #include "../include/file_io.h"
 #include "../include/graham_scan2.h"
+
+int main()
+{
+
+        Stack input;
+        Stack result;
+        stackCreate(&input);
+        stackCreate(&result);
+
+        importData(&input);
+
+        fastGrahamScan(&input, &result);
+
+        exportData(&result);
+
+        return 0;
+}
